5a82a6289b88630bde19e6e77c361a7e32ffe4d7
Kamal Rathore <mr.rathoreji52@gmail.com>
2026-01-15 22:31:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	let obj ={};
+
+    for(let char of str){
+      if(!obj[char]) obj[char] = 0;
+      obj[char]++;
+    }
+
+	for(let num in obj){
+      if(obj[num] === 1){
+        return num;
+      }
+    }
+	
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
